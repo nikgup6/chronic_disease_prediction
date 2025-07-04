64b11c2c1d23380ad73a7f0ded5ca4472bf7b99a
nikgup6 <nikunj.gupta_2026@woxsen.edu.in>
2025-07-04 18:14:17 +0530

    link updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -172,7 +172,7 @@ const App = () => {
       };
 
       // Send data to Flask backend for ML prediction
-      const response = await fetch('http://localhost:5000/predict', {
+      const response = await fetch('https://chronic-disease-prediction-1.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
